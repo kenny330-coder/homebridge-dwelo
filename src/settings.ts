4fd51b66ab037b1c79353941e427af5a46f566ee
Kenneth Chiara <kennethchiara@gmail.com>
2025-08-20 01:24:08 -0600

    Name Change

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomebridgePluginDwelo';
+export const PLATFORM_NAME = 'HomebridgeCustomPluginDwelo';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = '@eduardoramirez/homebridge-dwelo';
+export const PLUGIN_NAME = '@kenny330-coder/homebridge-custom-dwelo';
