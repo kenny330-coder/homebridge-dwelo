f11c2d5abd1cffed257e3b4951a9a09bc34f7b94
Kenneth Chiara <kennethchiara@gmail.com>
2025-08-20 02:40:38 -0600

    fixes

--- src/DweloDimmerAccessory.ts
+++ src/DweloDimmerAccessory.ts
@@ -7,7 +7,9 @@ import {
 import { DweloAPI, Sensor } from './DweloAPI';
 import { StatefulAccessory } from './StatefulAccessory';
 
-export class DweloDimmerAccessory extends StatefulAccessory {
+const POLLING_INTERVAL = 1 * 60 * 1000; // 1 minute
+
+export class DweloDimmerAccessory extends StatefulAccessory<[boolean, number]> {
   private readonly service: Service;
 
   constructor(log: Logging, api: API, dweloAPI: DweloAPI, accessory: PlatformAccessory) {
@@ -16,15 +18,29 @@ export class DweloDimmerAccessory extends StatefulAccessory {
     this.service = this.accessory.getService(this.api.hap.Service.Lightbulb) || this.accessory.addService(this.api.hap.Service.Lightbulb);
 
     this.service.getCharacteristic(this.api.hap.Characteristic.On)
-      .onGet(() => this.service.getCharacteristic(this.api.hap.Characteristic.On).value)
+      .onGet(() => {
+        if (this.desiredValue !== undefined && Date.now() - this.lastUpdated < POLLING_INTERVAL) {
+          return this.desiredValue[0];
+        }
+        return this.service.getCharacteristic(this.api.hap.Characteristic.On).value;
+      })
       .onSet(async value => {
+        this.desiredValue = [value as boolean, this.desiredValue?.[1] || 0];
+        this.lastUpdated = Date.now();
         await this.dweloAPI.toggleSwitch(value as boolean, this.accessory.context.device.uid);
         this.log.debug(`Dimmer state was set to: ${value ? 'ON' : 'OFF'}`);
       });
 
     this.service.getCharacteristic(this.api.hap.Characteristic.Brightness)
-      .onGet(() => this.service.getCharacteristic(this.api.hap.Characteristic.Brightness).value)
+      .onGet(() => {
+        if (this.desiredValue !== undefined && Date.now() - this.lastUpdated < POLLING_INTERVAL) {
+          return this.desiredValue[1];
+        }
+        return this.service.getCharacteristic(this.api.hap.Characteristic.Brightness).value;
+      })
       .onSet(async value => {
+        this.desiredValue = [this.desiredValue?.[0] || false, value as number];
+        this.lastUpdated = Date.now();
         await this.dweloAPI.setBrightness(value as number, this.accessory.context.device.uid);
         this.log.debug(`Dimmer brightness was set to: ${value}`);
       });
